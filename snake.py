8d9b9f7c3ce6a09a92d1e8fb87ac79f368596185
juan Carlos Romero <A01658390@tec.mx>
2024-03-21 15:28:27 -0600

    Se creó la funcionalidad que permite a la comida moverse un cuadro a la vez

--- snake.py
+++ snake.py
@@ -14,6 +14,7 @@ from turtle import *
 from freegames import square, vector
 
 food = vector(0, 0)
+food_direction = vector(0, -10) 
 snake = [vector(10, 0)]
 aim = vector(0, -10)
 
@@ -58,6 +59,23 @@ def move():
     ontimer(move, 100)
 
 
+def move_food():
+    """Move food one step at a time."""
+    global food_direction  # Utilizamos la dirección de la comida globalmente
+
+    next_food_pos = food + food_direction  # Calculamos la próxima posición de la comida
+
+    # Verificamos si la próxima posición está dentro de los límites
+    if -200 < next_food_pos.x < 190 and -200 < next_food_pos.y < 190:
+        food.x = next_food_pos.x
+        food.y = next_food_pos.y
+    else:
+        # Si la próxima posición está fuera de los límites, cambiamos la dirección aleatoriamente
+        food_direction = vector(randrange(-1, 2) * 10, randrange(-1, 2) * 10)
+
+    ontimer(move_food, 1000)  
+
+
 setup(420, 420, 370, 0)
 hideturtle()
 tracer(False)
@@ -67,4 +85,6 @@ onkey(lambda: change(-10, 0), 'Left')
 onkey(lambda: change(0, 10), 'Up')
 onkey(lambda: change(0, -10), 'Down')
 move()
+move_food() 
 done()
+
